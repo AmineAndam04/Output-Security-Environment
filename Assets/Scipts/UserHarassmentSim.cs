ed9f41b906808c781b5872a8026d5feb047b402f
AndamAmine <andamamine83@gmail.com>
2023-10-02 20:30:22 +0100

    Finished prototype

--- Assets/Scipts/UserHarassmentSim.cs
+++ Assets/Scipts/UserHarassmentSim.cs
@@ -1,39 +1,75 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-//using Unity.VisualScripting;
 
-public class UserHarassmentSim : MonoBehaviour {
-
-    public GameObject malicousAvatarPrefab ;
+public class UserHarassmentSim : MonoBehaviour
+{
+    public GameObject maliciousAvatarPrefab;
     private GameObject maliciousAvatar;
     private float spawnDelay = 5f;
-    private Vector3 initialPosition = new Vector3(0f,0f,0f);
+    private Vector3 initialPosition = new Vector3(0f, 0f, 0f);
 
     private GameObject[] otherAvatars;
-    public float  privateSpace = 1f;
+    public float privateSpace = 0.5f;
     private GameObject targetedAvatar;
-    public float attackPeriod = 10f ;
+    public float attackPeriod = 50f;
+    private GameObject previousTargetedAvatar;
 
-    
-
-    void   Start() {
+    void Start()
+    {
         Invoke("SpawnMaliciousAvatar", spawnDelay);
         otherAvatars = GameObject.FindGameObjectsWithTag("Avatar");
-        for (int i = 0; i < otherAvatars.Length; i++)
+    }
+
+    void SpawnMaliciousAvatar()
+    {
+        maliciousAvatar = Instantiate(maliciousAvatarPrefab, initialPosition, Quaternion.identity);
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        while (maliciousAvatar != null)
         {
-            Debug.Log(otherAvatars[i].transform.position);
+            // Randomly choose a new targeted avatar only when the previous attack is over.
+            if (targetedAvatar == null)
+            {
+                do
+                {
+                    targetedAvatar = otherAvatars[Random.Range(0, otherAvatars.Length)];
+                } while (targetedAvatar == previousTargetedAvatar); // Ensure the new target is not the same as the previous one
+
+                Debug.Log("New Target: " + targetedAvatar.transform.position);
+                //Debug.Log("Distance btw them is:" + Vector3.Distance(maliciousAvatar.transform.position,targetedAvatar.transform.position));
+                previousTargetedAvatar = targetedAvatar;
+            }
+
+            float distance = Vector3.Distance(maliciousAvatar.transform.position, targetedAvatar.transform.position);
+            
+
+            if (distance <    privateSpace)
+            {
+                
+                // Attack the target avatar.
+                Transform privateSpaceSphere = targetedAvatar.transform.Find("PrivateSpace"); 
+                Renderer sphereRenderer = privateSpaceSphere.GetComponent<Renderer>();
+                Color originalColor = sphereRenderer.material.color; 
+                sphereRenderer.material.color = new Color(1f, 0f, 0f, sphereRenderer.material.color.a);
+;
+                yield return new WaitForSeconds(attackPeriod);
+
+                // Reset the targeted avatar to null to choose a new target in the next attack.
+                targetedAvatar = null;
+                sphereRenderer.material.color = originalColor ; 
+            }
+            else
+            {
+                maliciousAvatar.transform.position = Vector3.Lerp(maliciousAvatar.transform.position, targetedAvatar.transform.position, Time.deltaTime);
+                Debug.Log("Distance" + distance);
+            }
+
+            yield return null;
         }
     }
-    void SpawnMaliciousAvatar(){
-        // Transform initialTransform ;
-        //initialTransform.position = initialPosition;
-
-        // Instantiate the malicious avatar at the predefined position
-        maliciousAvatar = Instantiate(malicousAvatarPrefab, initialPosition, Quaternion.identity);
-
-        
-    }
-
-
 }
+
